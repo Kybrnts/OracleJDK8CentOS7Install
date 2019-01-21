e71f1eae5d1677164adc60f3b608c5ebd71f21cb
Kybernetes <correodelkybernetes@gmail.com>
2019-01-21 12:18:01 -0300

    Kybrnts.- Small corrections Added and corrected comments Also s slight chnge in main In branch Master Changes requested for commit 	updated:    OracleJDK8CentOS7Install.sh

--- OracleJDK8CentOS7Install.sh
+++ OracleJDK8CentOS7Install.sh
@@ -4,8 +4,8 @@
 #: Author      : Kybernetes <correodelkybernetes@gmail.com>
 #: Version     : 1.00
 #: Description : Executable Dash shell script
-#:             : Installs Oracle's JDK8 in an Atos NOCUY Centos installation,
-#:             : from payload's zip compressed tarball.
+#:             : Installs Oracle's JDK8 in a CentOS v7 installation from pay-
+#:             : load's zip compressed tarball.
 #: Options     : -h : Displays help
 #:             : -u : Uninstall
 #: Usage       : 1. Create a copy of this script
@@ -17,8 +17,8 @@
 _RPLY=                                            ## Read builtin reply
 _USER=$USER                                       ## Current user name
 _NOWDATE=YYYY-mm-dd                               ## Current date
-_JDKPRNTDIR=/usr/java                             ## All javas container
-_JDKPYLDVRS=1.8.0_XXX                             ## Payload's jdk version
+_JDKPRNTDIR=/usr/java                             ## All Javas container
+_JDKPYLDVRS=1.8.0_XXX                             ## Payload's JDK8 version
 _SHPRFLENME=/etc/profile.d/java.sh                ## Shell profile script
 _SHPRFLBACK=~/${_SHPRFLENME##*/}/_${NOWDATE}.back ## Backups for above file
 _PYLDLINENM=0                                     ## Payload's first line
@@ -51,7 +51,7 @@ _CMD_ALTERNATIVES=/sbin/alternatives
     { printf "${0##*/}: %s not found\n" "$_CMD_ALTERNATIVES"; exit 1; }
 # Create alias to force use specific path within functions, while keeping syntax
 # short. It will expand on func. declaration NOT on invocation. But DON'T put it
-# inside "if" or any other onstruct command list: it could render it useless!!
+# inside "if" or any other construct command list: it could render it useless!!
 alias sleep="$_CMD_SLEEP" whoami="$_CMD_WHOAMI" cat="$_CMD_CAT"\
       grep="$_CMD_GREP" tail="$_CMD_TAIL" date="$_CMD_DATE" mkdir="$_CMD_MKDIR"\
       awk="$_CMD_AWK" tar="$_CMD_TAR" alternatives="$_CMD_ALTERNATIVES"
@@ -73,11 +73,11 @@ wrnmsg() { #@ DESCRIPTION: Print Warning messages to STDOUT
 }
 setUser() { #@ DESCRIPTION: Sets global _USER w/current user name.
             #@ USAGE: setUser
-    local user                                           ## Name local stoarge
+    local user                                           ## Name local storage
     if ! user=$(whoami 2>/dev/null); then                ## Try to get name
         printf "Error: Failed to get current user\n" >&2 ## Fail if not possible
         return 1                                         ## Return w/>0 status
-    fi                                                   ## Got username?
+    fi                                                   ## Got user name?
     _USER=$user                                          ## Yes so, Set global
 }
 setNowDate() { #@ DESCRIPTION: Used to set global _NOWDATE value w/today's date
@@ -109,8 +109,8 @@ setJdkPyldVers() { #@ DESCRIPTION: Sets global _JDKPYLDVRS with payload's JDK8
                    #@              version name.
                    #@ USAGE: usage setJdkPyldVers
     # Local variables ("local" must be function's first statement)
-    local vers r                 ## Jdk version and STDIN lines counter
-    r=0                          ## Initialize readed lines counter
+    local vers r                 ## JDK8 version and STDIN lines counter
+    r=0                          ## Initialize read lines counter
     while IFS= read -r _RPLY; do ## Read whole lines until EOF
         r=$((r + 1))             ## Increase read lines counter
         if [ $r -gt 1 ]; then    ## Allow only one line from STDIN
@@ -158,7 +158,7 @@ uninstallJdk8() { #@ DESCRIPTION: Removes Oracle JDK8 installation directory
     # Delete all files from installation path in coprocess and print its output
     # line by line in a single line that refresh itself
     while IFS= read -r _RPLY; do           ## Read whole current line
-        [ X"$_RPLY" = Xprintf ] ||         ## Reset curor's pos. and println
+        [ X"$_RPLY" = Xprintf ] ||         ## Reset cursor's pos. and println
             printf "\033[K\033[s%s\033[u" "$_RPLY"
         sleep 0.01                         ## Wait some time before next
     done <<EOF
@@ -169,23 +169,23 @@ EOF
         noerrs=false                                        ## whent wrong
     fi
     rm -vf "$_SHPRFLENME"                      ## Now remove shell profile
-    if [ -e "$_SHPRFLENME" ]; then             ## Chek if it still exists
-        errmsg "Failed to remove $_SHPRFLENME" ## If so, something whent wrong
+    if [ -e "$_SHPRFLENME" ]; then             ## Check if it still exists
+        errmsg "Failed to remove $_SHPRFLENME" ## If so, something went wrong
         noerrs=false
     fi
     $noerrs                                    ## Return w/errors if needed
 }
 installJdk8() { #@ DESCRIPTION: Installs Oracle's JdK8 from payload
                 #@ USAGE: installJdk8
-    # Decompress & untar all files to inistallation path in coprocess, and print
+    # Decompress & untar all files to installation path in coprocess, and print
     # output line by line in a single self refreshing one
     while IFS= read -r _RPLY; do ## Red whole current line
         case $_RPLY in           ## Check line
-            STATUS:\ *[0-9])     ## Matches an error upon untar coproc command
+            STATUS:\ *[0-9])     ## Matches an error upon untar coproc. command
                 errmsg "Failed to extract payload to $_JDKPRNTDIR"
                 return 1 ;;      ## Finish w/errors
             "") continue ;;      ## Do not print empty lines
-        esac                     ## Printf resets cursos position and println
+        esac                     ## Printf resets cursor's position and println
         printf "\033[K\033[sInstalled %s\033[u" "'$_JDKPRNTDIR/$_RPLY'"
         sleep 0.01               ## Wait some time before next line
     done <<EOF
@@ -193,7 +193,7 @@ $({ tail -n +$_PYLDLINENM "$0" |
        tar -C "$_JDKPRNTDIR" -xvzf -; } 2>/dev/null || 
   printf "STATUS: %d" $?)
 EOF
-    ## Coprocess starts reading $0 from payload first line onwards, sending out-
+    ## Coprocess starts reading $0 from payload first line onward, sending out-
     ## put to tar-unzip-extract command to specific path. If tar exit status >0
     ## it will be returned to STDOUT so it can be handled by reading while loop.
     printf "\n"                  ## Avoid re-write the last line
@@ -208,11 +208,11 @@ catcp() { #@ DESCRIPTION: Copy $1 filename in $2 filename using cat.
     fi
 }
 installShProfile() { #@ DESCRIPTION: Installs shell profile w/environment provi-
-                     #@              ded by new JDK installation. Tipically this
+                     #@              ded by new JDK installation. Typically this
                      #@              it will set your JAVA_HOME.
                      #@ USAGE: installShProfile
     local out             ## Write command output container
-    # Tru tp wrote tp $_SHPRFLENME shell profile saving attempts output
+    # Try to write to $_SHPRFLENME shell profile saving attempt's output
     out=$({ cat >"$_SHPRFLENME" <<EOF
 ##
 ## Add JAVA_HOME to environment (preserve any previously set value)
@@ -261,7 +261,7 @@ OPTIONS
 
 STATUS
 Exit status will be 0 when all operations completed successfully. Otherwise 1.
-Status >1 is used for syntas or input/output errors.
+Status >1 is used for syntax or input/output errors.
 
 EOF
     exit 0
@@ -270,16 +270,16 @@ EOF
 ## -- Main ---------------------------------------------------------------------
 main() {
     # Local variables ("local" must be function's first statement)
-    local uinm hlpm opt noinsterrs alt ## Mode flags, option errors flag, etc 
+    local uinm hlpm opt noinsterrs alt ## Mode flags, option errors flag, etc
     uinm=false hlpm=false              ## Initialize mode flags
     while getopts :UuHh opt; do        ## While we still have an option in args
         case $opt in                   ## Check option case
             [Uu])                      ## Uninstall mode selected?
                 if $uinm; then         ## If already selected finish w/errors
                     errmsg "\"$opt\" option already used"
-                    return 2           ## (Allow uninstal option only once)
+                    return 2           ## (Allow uninstall option only once)
                 fi
-                uinm=true ;;           ## Else set uninstal mode flag to true
+                uinm=true ;;           ## Else set uninstall mode flag to true
             [Hh])                      ## Help mode selected?
                 if $hlpm; then         ## If already selected finish w/errors
                     errmsg "\"$opt\" option already used"
@@ -291,7 +291,7 @@ main() {
                 return 2 ;;            ## Finish w/errors
         esac
     done
-    shift $(($OPTIND - 1))        ## Shift already parced arguments (opts)
+    shift $(($OPTIND - 1))        ## Shift already parsed arguments (opts)
     if [ $# -ne 0 ]; then         ## If additional non option args are present
         errmsg "Invalid argument" ## Finish w/errors
         return 2                  ## (No no-option args allowed)
@@ -313,13 +313,14 @@ main() {
     case $_USER in                  ## Check matching user name
         root) ;;                    ## If root, then OK..
         *)                          ## Other users are not allowed hence, finish
-            errmsg "$_USER is not allowed to continue"
+            errmsg "$_USER is not allowed to continue"\
+                   "Use \"sudo\", or log in as root and try again."
             return 1 ;;             ## With errors
     esac
     stdmsg "Done."                  ## User check finished successfully
     if $uinm; then # Uninstall mode --------------------------------------------
         noinsterr=true              ## Reset errors flag
-        # Check installation dir. existence before doint anything
+        # Check installation dir. existence before doing anything
         if [ ! -d "$_JDKPRNTDIR/$_JDKPYLDVRS" ]; then
             errmsg "JDK8 installation not found" ## If not found, display error
             stdmsg "Nothing to do"               ## But finish without errors
@@ -336,7 +337,7 @@ main() {
                 fi
                 stdmsg "Done."                   ## JDK8 uninstall finished
             else
-                stdmsg "Cancelled"               ## If "No", do not unistall
+                stdmsg "Cancelled"               ## If "No", do not uninstall
             fi                                   ## And let user know
             # Prompt user to proceed w/JDK common progs. alternatives removal
             if ynPrmpt "Remove alternatives for common programs"; then
@@ -351,9 +352,9 @@ main() {
                 done
                 stdmsg "Done"                    ## Alternatives rm completed
             else
-                stdmsg "Cancelled"               ## Alternatives rmo cancelled
-            fi  
-        fi      
+                stdmsg "Cancelled"               ## Alternatives rm cancelled
+            fi
+        fi
         $noinsterr ## Finish w/errors if flag indicates that something failed
     else           # Install mode ----------------------------------------------
                    # This workflow is insane, needs improvement/optimization
@@ -364,7 +365,7 @@ main() {
         # Check and create all javas container
         if [ ! -d "$_JDKPRNTDIR" ]; then   ## If container do not exists
             if [ -e "$_JDKPRNTDIR" ]; then ## But a file w/same name exists
-                errmsg "File $_JDKPRNTDIR already exists" 
+                errmsg "File $_JDKPRNTDIR already exists"
                 return 1                   ## Finish w/errors
             else                           ## Otherwise proceed w/installation
                 stdmsg "Java installation path does not exits"
@@ -391,82 +392,89 @@ main() {
             wrnmsg "Found previous installation of $_JDKPYLDVRS"
             # If previous installation found, prompt user to re-install
             if ynPrmpt "Would you like to re-install it"; then
-                if ! installJdk8; then
-                    noinsterrs=false
-                    stdmsg "Failed"
-                else
-                    stdmsg "Done"
+                if ! installJdk8; then ## If "yes" try to install
+                    noinsterrs=false   ## Set flag accordingly if install fails
+                    stdmsg "Failed"    ## Report failure to user
+                else                   ## Re-install succeeded
+                    stdmsg "Done"      ## Notify user
                 fi
-            else
+            else                       ## If "No" cancel do not re-install
                 stdmsg "Cancelled"
             fi
-        elif ! installJdk8; then
-            noinsterrs=false
-            stdmsg "Failed."
-        else
-            stdmsg "Done."
+            # If no previous installation found
+        elif ! installJdk8; then ## Proceed to install
+            noinsterrs=false     ## Set flag accordingly if install fails
+            stdmsg "Failed."     ## Report failure to user
+        else                     ## Install succeeded
+            stdmsg "Done."       ## Notify user
         fi
         # Try to install new shell profile script
-        stdmsg "Installing shell profile ${_SHPRFLENME##*/}.."
-        sleep 1
-        if [ -e "$_SHPRFLENME" ]; then
-            wrnmsg "Profile ${_SHPRFLENME##*/} found"
+        stdmsg "Installing shell profile ${_SHPRFLENME##*/}.." ## Notify user
+        sleep 1                                                ## Allow to read
+        if [ -e "$_SHPRFLENME" ]; then                ## If prfle already exists
+            wrnmsg "Profile ${_SHPRFLENME##*/} found" ## Warn user and prompt
             if ! ynPrmpt "Would you like to create backup"; then
-                stdmsg "Cancelled."
-            else
+                stdmsg "Cancelled."                   ## If "No" cancel install
+            else                                      ## If "Yes"
                 stdmsg "Creating $_SHPRFLBACK backup.."
-                if [ -e "$_SHPRFLBACK" ]; then
-                    wrnmsg "Backup already exists"
+                if [ -e "$_SHPRFLBACK" ]; then        ## Check if backup exists
+                    wrnmsg "Backup already exists"    ## Warn/prmpt to overwrite
                     if ! ynPrmpt "Do you want to overwrite it"; then
-                        stdmsg "Overwrite cancelled."
-                    else
+                        stdmsg "Overwrite cancelled." ## If "No" don't overwrite
+                    else                              ## If yes, proceed
                         stdmsg "Overwritting backup.."
                         sleep 1
+                        # Try to create the backup copy of shell profile
                         if ! catcp "$_SHPRFLENME" "$_SHPRFLBACK"; then
-                            noinsterrs=false
-                            stdmsg "Overwrite failed."
-                        else
-                            stdmsg "Overwrite one."
+                            noinsterrs=false           ## Copy fails?, set flag
+                            stdmsg "Overwrite failed." ## & notify user
+                        else                           ## Back-up copy made
+                            stdmsg "Overwrite done."
                         fi
                         stdmsg "Backup done."
                     fi
+                # If profile backup doesn't exist, do not prompt, and create it
                 elif ! catcp "$_SHPRFLENME" "$_SHPRFLBACK"; then
-                    noinsterrs=false
-                    stdmsg "Backup failed."
-                else
+                    noinsterrs=false        ## Copy fails?, set flag
+                    stdmsg "Backup failed." ## and notify user
+                else                        ## Back-up copy made
                     stdmsg "Backup Done."
                 fi
             fi
             wrnmsg "Profile ${_SHPRFLENME##*/} found"
+            # Since shell profile was found, also prompt to re-install it
             if ! ynPrmpt "Would you like to re-install it"; then
-                stdmsg "Cancelled"
-            else
+                stdmsg "Cancelled"               ## If "No", don't
+            else                                 ## If "Yes", try to reinstall
                 stdmsg "Re-installing profile.."
-                if ! installShProfile; then
-                    noinsterrs=false
-                    stdmsg "Re-install Failed."
-                else
+                if ! installShProfile; then      ## If profile re-install fails
+                    noinsterrs=false             ## Set flag accordingly
+                    stdmsg "Re-install Failed."  ## Notify user
+                else                             ## Profile re-install succeeded
                     stdmsg "Re-install Done."
                 fi
             fi
-        elif ! installShProfile; then
-            noinsterrs=false
-            stdmsg "Failed."
-        fi
+        # No previously installed shell profile found, so proceed to install
+        elif ! installShProfile; then ## If installation fails
+            noinsterrs=false          ## Set flag accordingly
+            stdmsg "Failed."          ## Notify user
+        fi                            ## profile install succeeded
         stdmsg "Done."
         $noinsterr
+        # Prompt user to proceed w/JDK common progs. alternatives installation
         if ynPrmpt "Install alternatives for common programs"; then
-            stdmsg "Installing alternatives.."
-            sleep 1
-            for alt in jar java javac javaws keytool; do
-                stdmsg "Alternative for program $alt"
-                sleep 0.5
+            stdmsg "Installing alternatives.."           ## If "yes", notify usr
+            sleep 1                                      ## Allow some time
+            for alt in jar java javac javaws keytool; do ## For each prog
+                stdmsg "Alternative for program $alt"    ## Tell user about it
+                sleep 0.5                                ## Allow some time
+                # Install current prog alternative w/a priority of 150
                 instJavaAlt $alt "$_JDKPRNTDIR/$_JDKPYLDVRS/bin/$alt" 150 ||
                     noinsterr=false
             done
         fi
-        stdmsg "Done"
-        $noinsterr
+        stdmsg "Done" ## Installation finished
+        $noinsterr    ## Finish w/flag's exit status
     fi
 }
 # Aliases no longer needed
